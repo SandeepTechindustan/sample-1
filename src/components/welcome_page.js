4172a734740e743c838992e0a2e5a56259c7009c
SandeepTechindustan <sandeep.techindustan@gmail.com>
2017-11-20 10:49:06 +0530

    added welcome page

---
+++ src/components/welcome_page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Welcome extends React.Component {
+	render() {
+		return (
+			<div>
+				<div>
+					<h1>Welcome User</h1>
+				</div>
+				<div>
+					<p>This is the first page..<br />
+					other components are comming soon....</p>
+				</div>
+			</div>
+			)
+	}
+}
